8328d7f12e8a4e29d5fd4205a2fefa8e896a7e0c
Florence JORNOD <fjornod@rddm503traitd.mnhn.fr>
2017-01-20 10:10:45 +0100

    cleaning

--- script/recursive_clustering_analyse.R
+++ script/recursive_clustering_analyse.R
@@ -8,11 +8,7 @@ require(MASS)
 # Exemple d'appel:
 # Rscript --no-save --no-restore --verbose recursive_clustering_analyse.R --kmer_file ../data/nt011630.rev.monomers.fst_149.length_166_177.kmers5 --matepair 0.96
 
-option_list = list(
-	make_option(c("--kmer_file"), action="store", default=NULL, type='character',
-							help="INPUT kmer table file"),
-	make_option(c("--matepair"), action="store", default=NULL, type='character',
-							help="Set mate pair threshold"))
+option_list = list(make_option(c("--kmer_file"), action="store", default=NULL, type='character',help="INPUT kmer table file"),make_option(c("--matepair"), action="store", default=NULL, type='character',help="Set mate pair threshold"))
 
 opt = parse_args(OptionParser(option_list=option_list))
 
@@ -22,7 +18,7 @@ get_kmer_table = function(input_file){
 }
 
 normalize_kmer = function (col_kmer){
-  return ( ((col_kmer)-mean(col_kmer)) / sd(col_kmer))
+	return ( ((col_kmer)-mean(col_kmer)) / sd(col_kmer))
 }
 
 input_species = function(input_file){
@@ -45,19 +41,17 @@ clustering_2 = function(data,size,nb_comp){
 	print(dim(data))
 	if (nrow(data) < size){
 		sample_1_id = rownames(data)
-	}
-	else {
+	}else{
 		sample_1_id = sample(rownames(data),size)
 	}
 	# prendre les 10% des valeurs propres
 	sample_1_dist = dist(data[sample_1_id,1:nb_comp],method = "euclidean") # avant data[sample_1_id,1:100] sauf que probleme quand moins de 100 colonnes..
-  hc_1 = hclust(d = sample_1_dist, method = "ward.D2")
+	hc_1 = hclust(d = sample_1_dist, method = "ward.D2")
 	hc_subtree_n = cutree(hc_1, 2)
 
 	if (nrow(data) < size){
 		return(list("classes" = hc_subtree_n, "subtree"  = hc_subtree_n, "DIST" = sample_1_dist))
-	}
-	else{
+	}else{
 		lda_1 = lda(x = data[sample_1_id,1:100],grouping = hc_subtree_n)
 		pred_lda = predict(lda_1, data[,1:100])
 		pred_lda$class = as.numeric(pred_lda$class)
@@ -75,9 +69,9 @@ test_selfpairmate = function(h_clust, mp){
 	f.mat[f.mat == 0] = 999
 	#print(f.mat)
 	result <- t(sapply(seq(nrow(f.mat)), function(i) {
- 		 j <- which.min(f.mat[i,])
-  		c(paste(rownames(f.mat)[i], colnames(f.mat)[j], sep='/'), f.mat[i,j])
-		}))
+		j <- which.min(f.mat[i,])
+		c(paste(rownames(f.mat)[i], colnames(f.mat)[j], sep='/'), f.mat[i,j])
+	}))
 	result = as.data.frame(result)
 	result$V1 = as.character(result$V1)
 	fhcsub.df = as.data.frame(h_clust$subtree,row.names = names(h_clust$subtree))
@@ -94,7 +88,7 @@ test_selfpairmate = function(h_clust, mp){
 	if ( (intra_AA > seuil)&(intra_BB > seuil)){
 		return(list("test" = 1, "intra_AA" = intra_AA, "intra_BB" =  intra_BB, "pairmate" = result$V1))
 	}
-	else{return(list("test" = 0, "intra_AA" = intra_AA, "intra_BB" =  intra_BB,"pairmate" = result$V1))}
+		else{return(list("test" = 0, "intra_AA" = intra_AA, "intra_BB" =  intra_BB,"pairmate" = result$V1))}
 }
 
 
@@ -102,8 +96,7 @@ test_selfpairmate = function(h_clust, mp){
 
 clusterize_me=function(data, n=0 , wkfile,filename, lim ,matepair) {
 	print("______________________________________________________")
-	print("dim:data:clusterize_me")
-	print(dim(data))
+
 	mycol = c("#002b36","#dc322f")
 	adj_col  = adjustcolor(col = mycol, alpha.f = 4/10)
 	palette(adj_col)
@@ -117,35 +110,32 @@ clusterize_me=function(data, n=0 , wkfile,filename, lim ,matepair) {
 	names(clusters_alt)=row.names(data)
 	fileid = filename
 	sub_data = data[,check_kmer != 0]
-	print("dim de sub_data")
-	print(dim(sub_data))
 	print(paste("START DEPTH =",n))
 	pca_1 = prcomp(sub_data)
 
-#	nb_comp=ncol(pca_1$x)/10
+	#	nb_comp=ncol(pca_1$x)/10
 	if(ncol(pca_1$x)>100){
 		nb_comp=100
 	}else{
 		nb_comp=ncol(pca_1$x)
 	}
+	print("Clustering")
 	clust_1 = clustering_2(pca_1$x, size = 20000,nb_comp)# size utile pour la lda
 	print("Done")
-	print("Clustering")
-		clusters = clust_1$classes
+	clusters = clust_1$classes
+	print("Test selfpairmate")
 	test = test_selfpairmate(clust_1, mp = matepair)
 	maintitle = paste(test$intra_AA,test$intra_BB)
-    labA = paste("PC1 (",round(pca_1$sdev[1],5)*100,"% )")
-    labB = paste("PC2 (",round(pca_1$sdev[2],5)*100,"% )")
-
+	labA = paste("PC1 (",round(pca_1$sdev[1],5)*100,"% )")
+	labB = paste("PC2 (",round(pca_1$sdev[2],5)*100,"% )")
+	print("Done")
+	print("PCA")
 	par(mar=c(5,5,4,2)+0.1)
 	#	plot(pca_1$x[, c(1,2)],main = maintitle,cex.axis = 1.25, cex.lab = 1.25, col=clust_1$classes,cex = 0.5 , pch = 16, xlab= labA, ylab = labB)
 	plot(pca_1$x[, c(1,2)],main = maintitle,cex.axis = 1.25, cex.lab = 1.25, col = clust_1$classes,cex = 1 , pch = 16, xlab= labA, ylab = labB)
 	plot(pca_1$x[, c(1,2)],main = maintitle,cex.axis = 1.25, cex.lab = 1.25,col = b_col,cex = 1 , pch = 16, xlab= labA, ylab = labB)
 	dev.off()
-print("###########################################")
-	print(fileid)
-	print("dim of data:")
-	print(dim(data))
+	print("###########################################")
 	print("proportions:")
 	print(paste(sum(clusters == 1),sum(clusters == 2)))
 	print("Pair Mate frequency:")
@@ -174,7 +164,9 @@ if ( !(is.null(opt$kmer_file) ) ) {
 	species = input_species(opt$kmer_file)
 	dir.create("results")
 	dir.create("results/plot")
-	file1 = paste("results/plot/",species,"_plot_pca",sep = '')
+	dirname=paste("results/plot/",opt$matepair,sep='')
+	dir.create(dirname)
+	file1 = paste(dirname,species,"_plot_pca",sep = '')
 	options(warn=1)
 	kmer_table = get_kmer_table(opt$kmer_file)
 	size = nrow(kmer_table)
